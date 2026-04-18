329f49d6c7ab3ca9365363bd2a8d1f24aea732f8
Atharv-Kondekar <atharvkondekar96@gmail.com>
2026-04-18 12:56:35 +0530

    Setup database connection using SQLAlchemy and env variables

---
+++ backend/app/config/database.py
@@ -0,0 +1,28 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+SessionLocal = sessionmaker(bind=engine)
+
+Base = declarative_base()
+
+
+# Dependency (important for routes later)
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
